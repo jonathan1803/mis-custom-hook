f38d74142a1aba743b8f4037c4fab12523f1bdbb
Jonathan Landaverde <jonathan.landaverde18@gmail.com>
2020-12-09 15:07:01 -0600

    Primer commit

---
+++ useForm.js
@@ -0,0 +1,22 @@
+import {useState} from 'react'
+//custom hook que se encargara de manejar los formularios
+export const useForm = (initialState= {}) => {
+ 
+const [values, setvalues] = useState(initialState)
+
+const reset = () =>{
+
+   setvalues(initialState)
+}
+
+const handleInputChange = ({target})=> {
+    setvalues({
+   
+       ...values,
+       [target.name]:target.value
+    })
+   }
+   
+
+   return [values, handleInputChange, reset];
+}
